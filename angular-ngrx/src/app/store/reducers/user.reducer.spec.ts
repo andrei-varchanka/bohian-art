74592ef6bbe032ae3871235dc2272c7d59bdd220
andrei-varchanka <53002740+andrei-varchanka@users.noreply.github.com>
2022-11-30 02:13:01 +0100

    user reducer tests

--- angular-ngrx/src/app/store/reducers/user.reducer.spec.ts
+++ angular-ngrx/src/app/store/reducers/user.reducer.spec.ts
@@ -1,6 +1,6 @@
 
 import { User } from "src/app/api/models";
-import { getUserAction, getUsersAction, getUsersErrorAction, getUsersSuccessAction, getUserSuccessAction, UserActions } from "../actions/user.actions";
+import { authAction, authErrorAction, authSuccessAction, changePasswordAction, changePasswordErrorAction, changePasswordSuccessAction, createUserAction, createUserErrorAction, createUserSuccessAction, deleteUserAction, deleteUserErrorAction, deleteUserSuccessAction, getUserAction, getUserErrorAction, getUsersAction, getUsersErrorAction, getUsersSuccessAction, getUserSuccessAction, updateUserAction, updateUserErrorAction, updateUserSuccessAction, UserActions } from "../actions/user.actions";
 import { UserState } from "../state/user.state";
 import { userReducer } from "./user.reducer";
 
@@ -33,6 +33,50 @@ const MOCK_DATA: User[] = [
   }
 ];
 
+const token = 'Bearer 889E15A70DFC70F854185C74C6BCFBDE89B8F098F5E3D2345B743DE1E5E86B97';
+
+describe('Auth reducer', () => {
+
+  it('should reduce the action AUTH', () => {
+      const action = authAction({email: MOCK_DATA[0].email, password: MOCK_DATA[0].password});
+      const newState = userReducer(state, action);
+      expect({ ...newState }).toEqual({...state, action: UserActions.AUTH, done: false, error: null});
+  });
+
+  it('should reduce the action AUTH_SUCCESS', () => {
+      const payload = {users: MOCK_DATA};
+      const action = authSuccessAction({token: token, user: MOCK_DATA[0]});
+      const newState = userReducer(state, action);
+      expect({ ...newState }).toEqual({...state, done: true});
+  });
+
+  it('should reduce the action AUTH_ERROR', () => {
+      const payload = new Error('Auth error');
+      const action = authErrorAction({error: payload});
+      const newState = userReducer(state, action);
+      expect({ ...newState }).toEqual({...state, done: true, error: payload});
+  });
+});
+
+describe('Create user reducer', () => {
+  it('should reduce the action CREATE_USER', () => {
+      const action = createUserAction({user: MOCK_DATA[0]});
+      const newState = userReducer(state, action);
+      expect({...newState}).toEqual({...state, action: UserActions.CREATE_USER, done: false, error: null});
+  });
+  it('should reduce the action CREATE_USER_SUCCESS', () => {
+      const action = createUserSuccessAction({token: token, user: MOCK_DATA[0]});
+      const newState = userReducer(state, action);
+      expect({...newState}).toEqual({ ...state, done: true, users: state.users?.concat(MOCK_DATA[0])});
+  });
+  it('should reduce the action CREATE_USER_ERROR', () => {
+      const payload = new Error('Create user error');
+      const action = createUserErrorAction({error: payload});
+      const newState = userReducer(state, action);
+      expect({...newState }).toEqual({...state, done: true, error: payload});
+  });
+});
+
 describe('Get users reducer', () => {
 
   it('should reduce the action GET_USERS', () => {
@@ -49,7 +93,7 @@ describe('Get users reducer', () => {
   });
 
   it('should reduce the action GET_USERS_ERROR', () => {
-      const payload = new Error('Error loading users');
+      const payload = new Error('Get users error');
       const action = getUsersErrorAction({error: payload});
       const newState = userReducer(state, action);
       expect({ ...newState }).toEqual({...state, done: true, error: payload});
@@ -70,8 +114,66 @@ describe('GET user by id reducer', () => {
       expect({...newState}).toEqual({ ...state, done: true, selectedUser: payload});
   });
   it('should reduce the action GET_USER_ERROR', () => {
-      const payload = new Error('Error loading the user');
-      const action = getUsersErrorAction({error: payload});
+      const payload = new Error('Get user error');
+      const action = getUserErrorAction({error: payload});
+      const newState = userReducer(state, action);
+      expect({...newState }).toEqual({...state, done: true, error: payload});
+  });
+});
+
+describe('Delete user reducer', () => {
+  it('should reduce the action DELETE_USER', () => {
+      const action = deleteUserAction({userId: MOCK_DATA[0].id});
+      const newState = userReducer(state, action);
+      expect({...newState}).toEqual({...state, action: UserActions.DELETE_USER, done: false, error: null});
+  });
+  it('should reduce the action DELETE_USER_SUCCESS', () => {
+      const action = deleteUserSuccessAction({userId: MOCK_DATA[0].id});
+      const newState = userReducer(state, action);
+      expect({...newState}).toEqual({ ...state, done: true, users: state.users?.filter(u => u.id != MOCK_DATA[0].id), selectedUser: null});
+  });
+  it('should reduce the action DELETE_USER_ERROR', () => {
+      const payload = new Error('Delete user error');
+      const action = deleteUserErrorAction({error: payload});
+      const newState = userReducer(state, action);
+      expect({...newState }).toEqual({...state, done: true, error: payload});
+  });
+});
+
+describe('Update user reducer', () => {
+  it('should reduce the action UPDATE_USER', () => {
+      const action = updateUserAction({user: MOCK_DATA[0]});
+      const newState = userReducer(state, action);
+      expect({...newState}).toEqual({...state, action: UserActions.UPDATE_USER, done: false, error: null, selectedUser: MOCK_DATA[0]});
+  });
+  it('should reduce the action UPDATE_USER_SUCCESS', () => {
+      const action = updateUserSuccessAction({user: MOCK_DATA[0]});
+      const newState = userReducer(state, action);
+      const index = state.users?.findIndex(user => user.id === state.selectedUser.id);
+      expect({...newState}).toEqual({ ...state, done: true, users: index && state.users ? [...state.users.slice(0, index), state.selectedUser, ...state.users.slice(index + 1)] : null});
+  });
+  it('should reduce the action UPDATE_USER_ERROR', () => {
+      const payload = new Error('Update user error');
+      const action = updateUserErrorAction({error: payload});
+      const newState = userReducer(state, action);
+      expect({...newState }).toEqual({...state, done: true, error: payload});
+  });
+});
+
+describe('Change password reducer', () => {
+  it('should reduce the action CHANGE_PASSWORD', () => {
+      const action = changePasswordAction({userId: MOCK_DATA[0].id, password: MOCK_DATA[0].password});
+      const newState = userReducer(state, action);
+      expect({...newState}).toEqual({...state, action: UserActions.CHANGE_PASSWORD, done: false, error: null});
+  });
+  it('should reduce the action CHANGE_PASSWORD_SUCCESS', () => {
+      const action = changePasswordSuccessAction({ user: MOCK_DATA[0]});
+      const newState = userReducer(state, action);
+      expect({...newState}).toEqual({ ...state, done: true});
+  });
+  it('should reduce the action CHANGE_PASSWORD_ERROR', () => {
+      const payload = new Error('Change password error');
+      const action = changePasswordErrorAction({error: payload});
       const newState = userReducer(state, action);
       expect({...newState }).toEqual({...state, done: true, error: payload});
   });
