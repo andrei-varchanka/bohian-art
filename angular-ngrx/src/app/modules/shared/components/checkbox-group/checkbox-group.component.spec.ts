2cb0a922d83cb913e91cf8466e8ec1870c5861e6
andrei-varchanka <53002740+andrei-varchanka@users.noreply.github.com>
2023-01-25 16:29:19 +0100

    checkbox group test

--- angular-ngrx/src/app/modules/shared/components/checkbox-group/checkbox-group.component.spec.ts
+++ angular-ngrx/src/app/modules/shared/components/checkbox-group/checkbox-group.component.spec.ts
@@ -17,9 +17,14 @@ describe('CheckboxGroupComponent', () => {
     fixture = TestBed.createComponent(CheckboxGroupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    spyOn(component.changeEvent, 'emit').and.callThrough();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should update selected items array on item selection', () => {
+    component.updateSelectedItems('A');
+    expect(component.selectedItems).toContain('A');
+    component.updateSelectedItems('A');
+    expect(component.selectedItems).not.toContain('A');
+    expect(component.changeEvent.emit).toHaveBeenCalledWith(component.selectedItems);
   });
 });
